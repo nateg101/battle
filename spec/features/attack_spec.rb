801145bd3e2aad1da8fcfd3ce5fc86d1a9890ea2
Nat Elliott-Green <natelliottgreen34@gmail.com>
2019-10-02 15:13:06 +0100

    Player 1 attack successful message implemented

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -2,7 +2,7 @@
 feature 'Attack a player' do
   scenario 'player1 attacks player2' do
     sign_in_and_play
-    click_button "Attack!"
+    click_button 'Attack!'
     expect(page).to have_content "Your attack was successful"
   end
 end
