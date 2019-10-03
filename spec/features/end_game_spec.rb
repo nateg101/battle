30c25f4490dad5398ca59a81bc4f70fb8290cabe
Nat Elliott-Green <natelliottgreen34@gmail.com>
2019-10-03 16:15:27 +0100

    button on attack pages can change to game over

--- spec/features/end_game_spec.rb
+++ spec/features/end_game_spec.rb
@@ -1,9 +1,11 @@
 feature 'End Game' do
   scenario 'It ends game when player has 0 hp' do
     sign_in_and_play
-    9.times do
+    8.times do
       attack_return
     end
+    click_button 'Attack!'
+    click_button 'Game Over'
     expect(page).to have_content 'Game Over'
   end
 end
